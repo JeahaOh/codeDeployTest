8045ec79963d3fd6e6a72685d55dbf5a9e1349aa
JeahaOh <jeaha1217@gmail.com>
2018-12-15 00:25:34 +0900

    .

--- src/main/java/bitcamp/java110/cms/dao/SceneReviewCmtDao.java
+++
@@ -1,8 +0,0 @@
-package bitcamp.java110.cms.dao;
-
-import bitcamp.java110.cms.domain.SceneReviewCmt;
-
-public interface SceneReviewCmtDao {
-  int insert(SceneReviewCmt comment);
-  void signOut1(int mno);
-}
